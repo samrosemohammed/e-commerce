1359a83472a189252de8ddcd20c640c96d485c0c
samrosemohammed <samrose.mohammed@gmail.com>
2025-08-10 22:06:54 +0545

    issue with nav

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,3 +1,4 @@
+"use client";
 import Link from "next/link";
 import { Search } from "lucide-react";
 import { Input } from "./ui/input";
@@ -5,8 +6,14 @@ import { UserDropDown } from "./UserDropDown";
 import { ThemeToggle } from "./ThemeToggle";
 import { NavShoppingCartIcon } from "./NavShoppingCartIcon";
 import { NavWishList } from "./NavWishList";
+import { usePathname } from "next/navigation";
+import { useSession } from "next-auth/react";
 
 export const NavBar = () => {
+  const pathname = usePathname();
+  const { data: session } = useSession();
+  if (pathname.startsWith("/dashboard")) return null;
+  if (session?.user.role === "admin") return null;
   return (
     <div className="shadow px-4 py-4 sticky top-0 bg-white dark:bg-background dark:border-b z-[10]">
       <nav className="max-w-screen-2xl mx-auto flex items-center justify-between">
